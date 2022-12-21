8c2b115d00d48db9b5d14817cb0f6ec51d56d2b9
Hussain Tech Labz <121178113+Husnain4444@users.noreply.github.com>
2022-12-21 19:42:32 +0500

    ,

---
+++ TapeRushProject/Assets/_ImageFill/Scripts/ImageFillScript.cs
@@ -0,0 +1,155 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ImageFillScript : MonoBehaviour
+{
+    public Transform FillParent;
+    public GameObject TapeObj;
+    public ParticleSystem[] ps;
+   
+    public List<Transform> allChildList;
+    List<Vector3> allChildPositionList=new List<Vector3>();
+    int currentNumber=0;
+    int currentImage;
+    void Start()
+    {
+        Init();
+    }
+    void Init()
+    {
+        allChildList = new List<Transform>();
+        foreach (Transform child in FillParent)
+        {
+            allChildList.Add(child);
+            allChildPositionList.Add(child.position);
+            child.gameObject.SetActive(false);
+        }
+        allChildList.RemoveAt(allChildList.Count - 1);
+        allChildPositionList.RemoveAt(allChildPositionList.Count - 1);
+
+        //Check Current Image
+        currentImage = PlayerPrefs.GetInt("Image");
+        for(int i=0;i<allChildList.Count;i++)
+        {
+            allChildList[i].GetComponent<Image>().sprite = UIManager.instance.FillImageList[currentImage].FillImageSpritList[i];
+        }
+
+        //Check Previous OpenPart
+        currentNumber =PlayerPrefs.GetInt("Child");
+        if (currentNumber > 0)
+        {
+            for (int i = 0; i < currentNumber; i++)
+            {
+                allChildList[i].gameObject.SetActive(true);
+            }
+        }
+        if (UIManager.instance.TotalChildAdd > 0)
+        {
+            StartCoroutine(AutoCombineCorutine());
+        }
+        
+    }
+    
+    //private void Update()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        Debug.Log("Mouse Clicked!");
+    //        CombineChild();
+    //    }
+    //}
+    IEnumerator AutoCombineCorutine()
+    {
+        for (int i = 0; i < UIManager.instance.TotalChildAdd; i++)
+        {
+            
+            CombineChild();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+    void CombineChild()
+    {
+       
+        //Should Not out of bound
+        if (currentNumber < allChildList.Count)
+        {
+            StartCoroutine(CombineCoroutine());
+        }
+        else 
+        {
+            Debug.Log("Picture is Completed!");
+        }
+    }
+    IEnumerator CombineCoroutine()
+    {
+        Transform obj = allChildList[currentNumber];
+        obj.position = obj.position + new Vector3(500, 0, 0);
+        obj.gameObject.SetActive(true);
+        obj﻿﻿﻿.DOMove(allChildPositionList[currentNumber], .4f);
+        yield return new WaitForSeconds(.2f);
+        //Vertical Combine
+        if (currentNumber % 5 == 0 && currentNumber!=0)
+        {
+            TapeObj.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+            TapeObj.transform.position = (allChildPositionList[currentNumber] + allChildPositionList[currentNumber - 5]) / 2;
+        }
+        // Horizontal Combine
+        else if(currentNumber!=0)
+        {
+            TapeObj.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            TapeObj.transform.position = (allChildPositionList[currentNumber] + allChildPositionList[currentNumber - 1]) / 2;
+        }
+        TapeObj.transform.localScale = Vector3.one;
+        TapeObj.SetActive(true);
+        yield return new WaitForSeconds(.1f);
+        TapeObj.transform.DOScaleY(0, .2f);
+        if (currentNumber < allChildList.Count - 1)
+        {
+            currentNumber++;
+            PlayerPrefs.SetInt("Child", currentNumber);
+        }
+        else
+        {
+            NextImage();
+            foreach (ParticleSystem s in ps)
+            {
+                s.Play();
+            }
+            
+            
+            
+        }
+        
+    }
+    IEnumerator delay()
+    {
+        yield return new WaitForSeconds(0.2f);
+        
+    }
+    void NextImage()
+    {
+        currentImage++;
+        if (currentImage > PlayerPrefs.GetInt("GiftIndex"))
+        {
+            PlayerPrefs.SetInt("GiftIndex", currentImage);
+        }
+        if (currentImage >= UIManager.instance.FillImageList.Length)
+        {
+            currentImage--;
+        }
+        PlayerPrefs.SetInt("Image", currentImage);
+        StartCoroutine("delay");
+        for (int i = 0; i < allChildList.Count; i++)
+        {
+            allChildList[i].gameObject.SetActive(false);
+            allChildList[i].GetComponent<Image>().sprite = UIManager.instance.FillImageList[currentImage].FillImageSpritList[i];
+        }
+        currentNumber = 0;
+        PlayerPrefs.SetInt("Child", currentNumber);
+    }
+
+    
+
+}
---
+++ TapeRushProject/Assets/_ImageFill/Scripts/IronSourceScript.cs
@@ -0,0 +1,240 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public enum rewardType { cash, secondChance, skin }
+public class IronSourceScript : MonoBehaviour
+{
+    public static IronSourceScript instance;
+    public rewardType currentReward;
+
+    //my testing(16d69d005) 1493bd7d9
+    //   string appKey = "13e7543c1";
+
+#if UNITY_ANDROID
+    string appKey = "";
+#elif UNITY_IPHONE
+    string appKey = "1493bd7d9";
+#else
+    string appKey = "";
+#endif
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+        PlayerPrefs.SetInt("noAds", 0);
+    }
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("noAds") == 1)
+        {
+            return;
+        }
+        BannerInit();
+        IronSourceEvents.onRewardedVideoAdOpenedEvent += RewardedVideoAdOpenedEvent;
+        IronSourceEvents.onRewardedVideoAdClickedEvent += RewardedVideoAdClickedEvent;
+        IronSourceEvents.onRewardedVideoAdClosedEvent += RewardedVideoAdClosedEvent;
+        IronSourceEvents.onRewardedVideoAvailabilityChangedEvent += RewardedVideoAvailabilityChangedEvent;
+        IronSourceEvents.onRewardedVideoAdStartedEvent += RewardedVideoAdStartedEvent;
+        IronSourceEvents.onRewardedVideoAdEndedEvent += RewardedVideoAdEndedEvent;
+        IronSourceEvents.onRewardedVideoAdRewardedEvent += RewardedVideoAdRewardedEvent;
+        IronSourceEvents.onRewardedVideoAdShowFailedEvent += RewardedVideoAdShowFailedEvent;
+    }
+
+    void BannerInit()
+    {
+        //Dynamic config example
+        IronSourceConfig.Instance.setClientSideCallbacks(true);
+
+        string id = IronSource.Agent.getAdvertiserId();
+        Debug.Log("unity-script: IronSource.Agent.getAdvertiserId : " + id);
+
+        Debug.Log("unity-script: IronSource.Agent.validateIntegration");
+        IronSource.Agent.validateIntegration();
+
+        Debug.Log("unity-script: unity version" + IronSource.unityVersion());
+        // SDK init
+        Debug.Log("unity-script: IronSource.Agent.init");
+        //  IronSource.Agent.init(appKey);
+        IronSource.Agent.init(appKey, IronSourceAdUnits.REWARDED_VIDEO, IronSourceAdUnits.INTERSTITIAL, IronSourceAdUnits.OFFERWALL, IronSourceAdUnits.BANNER);
+        //IronSource.Agent.initISDemandOnly (appKey, IronSourceAdUnits.REWARDED_VIDEO, IronSourceAdUnits.INTERSTITIAL);
+        // Add Banner Events
+        IronSourceEvents.onBannerAdLoadedEvent += BannerAdLoadedEvent;
+        IronSourceEvents.onBannerAdLoadFailedEvent += BannerAdLoadFailedEvent;
+        IronSourceEvents.onBannerAdClickedEvent += BannerAdClickedEvent;
+        IronSourceEvents.onBannerAdScreenPresentedEvent += BannerAdScreenPresentedEvent;
+        IronSourceEvents.onBannerAdScreenDismissedEvent += BannerAdScreenDismissedEvent;
+        IronSourceEvents.onBannerAdLeftApplicationEvent += BannerAdLeftApplicationEvent;
+
+
+
+
+        //Set User ID For Server To Server Integration
+        //// IronSource.Agent.setUserId ("UserId");
+
+        // Load Banner example
+        IronSource.Agent.loadBanner(IronSourceBannerSize.SMART, IronSourceBannerPosition.BOTTOM);
+        IronSource.Agent.loadInterstitial();
+    }
+    //void BannerShow()
+    //{
+    //    if (PlayerPrefs.GetInt("noAds") == 1)
+    //    {
+    //        return;
+    //    }
+
+    //    IronSource.Agent.loadBanner(IronSourceBannerSize.BANNER, IronSourceBannerPosition.BOTTOM, (string)"DefaultBanner");
+    //}
+
+
+
+
+    //Banner
+    void OnApplicationPause(bool isPaused)
+    {
+        Debug.Log("unity-script: OnApplicationPause = " + isPaused);
+        IronSource.Agent.onApplicationPause(isPaused);
+    }
+
+    //Banner Events
+    void BannerAdLoadedEvent()
+    {
+        Debug.Log("unity-script: I got BannerAdLoadedEvent");
+    }
+
+    void BannerAdLoadFailedEvent(IronSourceError error)
+    {
+        Debug.Log("unity-script: I got BannerAdLoadFailedEvent, code: " + error.getCode() + ", description : " + error.getDescription());
+    }
+
+    void BannerAdClickedEvent()
+    {
+        Debug.Log("unity-script: I got BannerAdClickedEvent");
+    }
+
+    void BannerAdScreenPresentedEvent()
+    {
+        Debug.Log("unity-script: I got BannerAdScreenPresentedEvent");
+    }
+
+    void BannerAdScreenDismissedEvent()
+    {
+        Debug.Log("unity-script: I got BannerAdScreenDismissedEvent");
+    }
+
+    void BannerAdLeftApplicationEvent()
+    {
+        Debug.Log("unity-script: I got BannerAdLeftApplicationEvent");
+    }
+
+
+
+
+
+
+
+    //Invoked when the RewardedVideo ad view has opened.
+    //Your Activity will lose focus. Please avoid performing heavy 
+    //tasks till the video ad will be closed.
+    void RewardedVideoAdOpenedEvent()
+    {
+    }
+    //Invoked when the RewardedVideo ad view is about to be closed.
+    //Your activity will now regain its focus.
+    void RewardedVideoAdClosedEvent()
+    {
+    }
+    //Invoked when there is a change in the ad availability status.
+    //@param - available - value will change to true when rewarded videos are available. 
+    //You can then show the video by calling showRewardedVideo().
+    //Value will change to false when no videos are available.
+    void RewardedVideoAvailabilityChangedEvent(bool available)
+    {
+        //Change the in-app 'Traffic Driver' state according to availability.
+        bool rewardedVideoAvailability = available;
+    }
+
+    //Invoked when the user completed the video and should be rewarded. 
+    //If using server-to-server callbacks you may ignore this events and wait for 
+    // the callback from the  ironSource server.
+    //@param - placement - placement object which contains the reward data
+    void RewardedVideoAdRewardedEvent(IronSourcePlacement placement)
+    {
+        //Check waht will be the Reward
+        switch (currentReward)
+        {
+            case rewardType.cash:
+                UIManager.instance.OnClaimClick();
+                break;
+            case rewardType.secondChance:
+                gameOverScript.instance.Restart();
+                break;
+            case rewardType.skin:
+                break;
+
+
+        }
+
+        // FindObjectOfType<GameManager>().RevivePlayer();
+    }
+    //Invoked when the Rewarded Video failed to show
+    //@param description - string - contains information about the failure.
+    void RewardedVideoAdShowFailedEvent(IronSourceError error)
+    {
+    }
+
+    // ----------------------------------------------------------------------------------------
+    // Note: the events below are not available for all supported rewarded video ad networks. 
+    // Check which events are available per ad network you choose to include in your build. 
+    // We recommend only using events which register to ALL ad networks you include in your build. 
+    // ----------------------------------------------------------------------------------------
+
+    //Invoked when the video ad starts playing. 
+    void RewardedVideoAdStartedEvent()
+    {
+    }
+    //Invoked when the video ad finishes playing. 
+    void RewardedVideoAdEndedEvent()
+    {
+    }
+    //Invoked when the video ad is clicked. 
+    void RewardedVideoAdClickedEvent(IronSourcePlacement placement)
+    {
+    }
+
+    public void InterstitialShow()
+    {
+        if (PlayerPrefs.GetInt("noAds") == 1)
+        {
+            return;
+        }
+        if (IronSource.Agent.isInterstitialReady())
+        {
+            IronSource.Agent.showInterstitial();
+        }
+        else
+        {
+            IronSource.Agent.loadInterstitial();
+            Debug.Log("unity-script: IronSource.Agent.isInterstitialReady - False");
+        }
+    }
+    public void RewadedAd()
+    {
+        if (IronSource.Agent.isRewardedVideoAvailable())
+        {
+            IronSource.Agent.showRewardedVideo();
+        }
+        else
+        {
+            Debug.Log("unity-script: IronSource.Agent.isRewardedVideoAvailable - False");
+        }
+    }
+
+}
